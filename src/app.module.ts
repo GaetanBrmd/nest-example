19d168d588064e5620479125cf9cf3be304eda93
GaetanBrmd <ga.bremond@gmail.com>
2022-05-05 18:40:57 +0200

    feat(config): add config service to load env var
    Implement ConfigService to access and validate environment variables
    across the application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { configuration } from 'config/configuration';
+import { validationSchema } from 'config/validation';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/config/env/${process.env.NODE_ENV}.env`,
+      load: [configuration],
+      validationSchema,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
