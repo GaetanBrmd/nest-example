78d9f4b823058562d9fac850a05228a661c2e832
Gaëtan BREMOND <gbremond@ippon.fr>
2022-06-23 11:11:26 +0200

    test: mock repositories and optimize Jest

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Post } from './models/post.entity';
+import { User } from './models/user.entity';
+
+export type MockType<T> = {
+  [P in keyof T]?: jest.Mock<{}>;
+};
+
+export const repositoryMockFactory: () => MockType<Repository<any>> = jest.fn(
+  () => ({
+    findOne: jest.fn((entity) => entity),
+    // ...
+  }),
+);
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +23,23 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: getRepositoryToken(User),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: getRepositoryToken(Post),
+          useFactory: repositoryMockFactory,
+        },
+        {
+          provide: 'KAFKA_CLIENT',
+          useValue: {
+            emit: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
